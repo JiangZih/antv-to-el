05ea85ccac7179dbc3e009eca734028b362edb39
jiangzihao@kanzhun.com <jiangzihao@kanzhun.com>
2023-04-17 09:21:24 +0800

    编辑

--- src/main/java/com/rule/parser/Flow.java
+++ src/main/java/com/rule/parser/Flow.java
@@ -5,12 +5,17 @@ import java.util.List;
 
 import lombok.Data;
 
+/**
+ * @author jiangzihao@kanzhun.com
+ */
+
 @Data
 public class Flow {
-	private List<Node> nodes;
-	public static Flow getInstance() {
-		Flow flow = new Flow();
-		flow.setNodes(new ArrayList<Node>());
-		return flow;
-	}
+    private List<Node> nodes;
+
+    public static Flow getInstance() {
+        Flow flow = new Flow();
+        flow.setNodes(new ArrayList<Node>());
+        return flow;
+    }
 }
